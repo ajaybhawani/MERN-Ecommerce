99065a7695a27b0c9519c9a31558cf7664f7296a
Ajay kumar <ajay.k@kodehash.com>
2026-08-16 17:18:39 +0530

    create login api

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,7 +1,8 @@
 import User from "../models/User.js";
-
 import bcrypt from "bcryptjs";
+import jwt from "jsonwebtoken";
 
+//sign up user
 export const signupUser = async (req, res) => {
   try {
     const { name, email, password } = req.body;
@@ -19,6 +20,39 @@ export const signupUser = async (req, res) => {
       password: hashPassword,
     });
     res.status(201).json({ message: "User register successfully" });
+  } catch (error) {
+    return res.status(500).json({ message: "Server Error:", error });
+  }
+};
+
+//login user
+export const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // check for user already exists
+    const user = await User.findOne({ email });
+
+    if (!user) {
+      return res.status(400).json({ message: "User not found" });
+    }
+
+    //compare password
+    const match = await bcrypt.compare(password, user.password);
+    if (!match) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    // genrate jwt token
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    res.json({
+      message: "Login successfully",
+      token,
+      user: { id: user._id, name: user.name, email: user.email },
+    });
   } catch (error) {
     return res.status(500).json({ message: "Server Error:", error });
   }
